2a0216de13f0ee795b02ff4556a5156e2a78ba7c
julianafood <chojgy@gmail.com>
2019-04-18 23:54:53 -0400

    commit

---
+++ routing/apiRoutes.js
@@ -0,0 +1,14 @@
+var friend = require("../data/friends");
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+  
+  app.post("/api/friends", function(req, res) {
+    friendData.push(req.body);
+    res.json(friends);
+    
+  });
+};
